215c00422f2a71ec59bd73cad63c1ac587717ae7
Martin Nándor Csíkos <csikos.martin17@gmail.com>
2023-04-02 15:39:50 +0200

    added input sanitization and validation

--- php/login.php
+++ php/login.php
@@ -1,43 +1,20 @@
 <?php
-//databaseConnection.php importálása
 require "databaseConnection.php";
-
-//connect függvény meghívása
 connect();
 
-//új felhasználó hozzáadása
-function addNewUser() {
-    //A method=POST-os form inputjainak eltárolása egy változóban readability miatt
-    $email = $_POST["email"];
-    $fhnev = $_POST["fhnev"];
-    $jelszo = $_POST["jelszo"];
-    $veznev = $_POST["veznev"];
-    $kernev = $_POST["kernev"];
-    $szuldatum = $_POST["szuldatum"];
-    $nem = $_POST["nem"];
+$fhnev = $_POST['fhnev'];
+$jelszo = $_POST['jelszo'];
 
-    //Ha a formon belül minden inputba került adat
-    if (isset($email) && isset($fhnev) && isset($jelszo) && isset($veznev) && isset($kernev) && isset($szuldatum) && isset($nem)) {
-        //Jelszó hashelése --> biztonsági okok miatt
-        //$jelszo a formon belül megadott jelszó, PASSWORD_DEFAULT a hashelés metódusa
-        $hasheltJelszo = password_hash($jelszo, PASSWORD_DEFAULT);
-    
-        //SQL adatbázisba, a users táblába bekerülnek az adatok
-        //Query = utasítás
-        //(email, fhnev, jelszo...) --> az email, fhnev, jelszo...sorokba kerülnek az értékek
-        //VALUES ($email, $fhnev...) --> a formban megadott adatok
-        $sqlQuery = "INSERT INTO users (email, fhnev, jelszo, veznev, kernev, szuldatum, nem) VALUES ($email, $fhnev, $hasheltJelszo, $veznev, $kernev, $szuldatum, $nem)";
-        
-        //Ha sikerült elvégezni az utasítást (bekerültek az adatok a táblába)
-        if ($csatlakozas->query($sqlQuery) === TRUE) {
-            //Átirányítás a bejelentkezett oldalra
-        } else {
-            echo "Hiba: " . $sql . "<br>" . $csatlakozas->error;
+if (isset($fhnev) && isset($jelszo)) {
+    $sqlQuery = "SELECT * FROM users";
+
+    foreach ($sqlQuery as $felhasznalo) {
+        if ($felhasznalo == $fhnev && password_verify($jelszo, $felhasznalo['jelszo'])) {
+            //Átirányítás
+            break;
         }
     }
-
-    //Adatbázis csatlakozásának megszüntetése
-    disconnect();
 }
 
+disconnect();
 ?>
---
+++ php/registration.php
@@ -0,0 +1,87 @@
+<?php
+//databaseConnection.php importálása
+require "databaseConnection.php";
+
+//connect függvény meghívása a databaseConnection-ből
+connect();
+
+//A method=POST-os form inputjainak eltárolása egy változóban
+//Ezeket a változókat megtisztítjuk az egyéb nem oda illő karakterektől
+$email = trim($_POST["email"], " ");
+$fhnev = trim($_POST["fhnev"], " ");
+$jelszo = trim($_POST["jelszo"], " ");
+$veznev = trim($_POST["veznev"], " ");
+$kernev = trim($_POST["kernev"], " ");
+$szuldatum = filter_var(trim($_POST["szuldatum"], " "), FILTER_SANITIZE_EMAIL);
+$nem = $_POST["nem"];
+
+$hibak[] = "";
+
+
+//Ha a formon belül minden inputba került adat
+if (isset($email) && isset($fhnev) && isset($jelszo) && isset($veznev) && isset($kernev) && isset($szuldatum) && isset($nem)) {
+    //Jelszó hashelése --> biztonsági okok miatt
+    //$jelszo a formon belül megadott jelszó, PASSWORD_DEFAULT a hashelés metódusa
+    $hasheltJelszo = password_hash($jelszo, PASSWORD_DEFAULT);
+
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $hibak[] = "Hibás e-mail formátum.";
+    }
+    if (strlen($email) > 255) {
+        $hibak[] = "Túl hosszú az e-mail cím. Maximum 255 karakterből kell állnia.";
+    }
+    if (strlen($fhnev) > 255 || strlen($fhnev) < 3) {
+        $hibak[] = "Túl hosszú vagy rövid a felhasználónév. Minimum 3, maximum 255 karakterből kell állnia.";
+    }
+    if (strlen($jelszo) < 6 || strlen($jelszo) > 255) {
+        $hibak[] = "Túl hosszú vagy rövid a jelszó. Minimum 6, maximum 255 karakterből kell állnia.";
+    }
+    if (strlen($veznev) < 2 || strlen($veznev) > 255) {
+        $hibak[] = "Túl hosszú vagy rövid a keresztnév. Minimum 2, maximum 255 karakterből kell állnia.";
+    }
+    if (strlen($kernev) < 2 || strlen($kernev) > 255) {
+        $hibak[] = "Túl hosszú vagy rövid a vezetéknév. Minimum 2, maximum 255 karakterből kell állnia.";
+    }
+    if (!datumValidalas($szuldatum)) {
+        $hibak[] = "Hibás dátum formátum. Helyes formátum: YYYY.MM.DD";
+    }
+    if ($nem != "ferfi" || $nem != "no" || $nem != "egyeb") {
+        $hibak[] = "Hibás nem. Az érték csak férfi, nő, vagy egyéb lehet.";
+    }
+
+
+    //Ha nincs semmilyen hiba
+    if (empty($hibak)) {
+        //SQL adatbázisba, a users táblába bekerülnek az adatok
+        //Query = utasítás
+        //(email, fhnev, jelszo...) --> az email, fhnev, jelszo...sorokba kerülnek az értékek
+        //VALUES ($email, $fhnev...) --> a formban megadott adatok
+        $sqlQuery = "INSERT INTO users (email, fhnev, jelszo, veznev, kernev, szuldatum, nem) VALUES ($email, $fhnev, $hasheltJelszo, $veznev, $kernev, $szuldatum, $nem)";
+
+        //Ha sikerült elvégezni az utasítást (bekerültek az adatok a táblába)
+        if ($csatlakozas->query($sqlQuery) === TRUE) {
+            //Átirányítás a bejelentkezett oldalra
+        } else {
+            echo "Hiba: " . $sqlQuery . "<br>" . $csatlakozas->error;
+        }
+    } else {
+        //Végigmegyünk a hibákon, és kiiratjuk
+        foreach ($hibak as $hiba) {
+            echo $hiba;
+        }
+    }
+}
+
+//Adatbázis csatlakozásának megszüntetése
+disconnect();
+
+//A születési dátum validálására szolgáló function
+function datumValidalas ($datum, $format = 'Y.m.d') {
+    //$d változóban létrehozunk egy formátumot
+    $d = DateTime::createFromFormat($format, $datum);
+
+    //ha a létrehozott formátum az megegyezik az átadott dátum formátumával, akkor igaz lesz, ha nem, hamis
+    return $d && $d->format($format) === $datum;
+}
+?>
