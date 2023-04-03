f8e4dae3e40db9a2f1408354529ec65087468e6c
Martin Nándor Csíkos <csikos.martin17@gmail.com>
2023-04-03 15:57:30 +0200

    added logout

---
+++ php/logout.php
@@ -0,0 +1,11 @@
+<?php
+require "session.php";
+
+$kijelentkezes = $_POST['kijelentkezes'];
+
+//Ha a kijelentkezés gombra nyomunk, töröljük a sessiont, és visszairányítjuk a usert a főoldalra
+if (isset($kijelentkezes)) {
+    stopSession();
+    //header('Location: asd.php');
+}
+?>
