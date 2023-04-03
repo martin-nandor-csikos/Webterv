86c280fc5a93f6d7bfa7d0895097f293557d245c
Martin Nándor Csíkos <csikos.martin17@gmail.com>
2023-04-03 10:50:46 +0200

    filled users table with data

--- etc/kutyamenhely.sql
+++ etc/kutyamenhely.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 01, 2023 at 08:29 PM
+-- Generation Time: Apr 03, 2023 at 10:49 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -29,21 +29,27 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL COMMENT 'Primary kulcs',
-  `email` varchar(255) NOT NULL COMMENT 'User e-mail címe',
+  `jogosultsag` varchar(30) NOT NULL DEFAULT 'user' COMMENT 'Felhasználó jogosultsága (admin/user)',
+  `email` varchar(255) DEFAULT NULL COMMENT 'User e-mail címe',
   `fhnev` varchar(255) NOT NULL COMMENT 'Felhasználónév',
   `jelszo` varchar(255) NOT NULL COMMENT 'Hashelt jelszó',
-  `veznev` varchar(255) NOT NULL COMMENT 'Vezetéknév',
-  `kernev` varchar(255) NOT NULL COMMENT 'Keresztnév',
-  `szuldatum` date NOT NULL COMMENT 'Születési dátum',
-  `nem` varchar(255) NOT NULL COMMENT 'Nem'
+  `veznev` varchar(255) DEFAULT NULL COMMENT 'Vezetéknév',
+  `kernev` varchar(255) DEFAULT NULL COMMENT 'Keresztnév',
+  `szuldatum` date DEFAULT NULL COMMENT 'Születési dátum',
+  `nem` varchar(255) DEFAULT NULL COMMENT 'Nem'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci COMMENT='Regisztrált felhasználók';
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `fhnev`, `jelszo`, `veznev`, `kernev`, `szuldatum`, `nem`) VALUES
-(1, 'teszt@teszt.hu', 'tesztUsername', 'asd', 'A', 'Sd', '2000-01-01', 'ferfi');
+INSERT INTO `users` (`id`, `jogosultsag`, `email`, `fhnev`, `jelszo`, `veznev`, `kernev`, `szuldatum`, `nem`) VALUES
+(3, 'admin', NULL, 'admin', '$2y$10$BwGVNMgJAcpBQis6JuCjjOlEqR5zkvQaygPu3nf.fAXCNyvsrliiy', NULL, NULL, NULL, NULL),
+(4, 'user', 'jeno@asd.hu', 'jeno1', '$2y$10$nBkBQ8BY59DT0oo/ltALBeBPpQI5Ea6jSExjispKVr6ORArMjPfaG', 'Kovács', 'Jenő', '1980-01-23', 'ferfi'),
+(5, 'user', 'david@asd.hu', 'DavidxHUN', '$2y$10$NXYY3mOVCQZul8jEtPbZeujku9jGdnuyNwqYjnmntzFfx59WREDTa', 'Boros', 'Dávid', '2010-08-03', 'ferfi'),
+(6, 'user', 'kerkriszta@mlm.hu', 'KerekesKrisztaMLM', '$2y$10$pnIn4ZFjsQMdVhNDG1BU7OkFkgQJTakbiXIUMz1dtJkKgw0WtRS1i', 'Kerekes', 'Kriszta', '2000-07-29', 'no'),
+(7, 'user', 'forgoeszti@asd.hu', 'eszti1997', '$2y$10$S8D82sQoLswUN6.edqnqmu4OBLouQHSUc9Va7PEvsLFdtTlQqNx2e', 'Forgó', 'Eszter', '1997-12-30', 'no'),
+(8, 'user', 'vargagabi@asd.hu', 'gabi123', '$2y$10$J43eCWI4r5JyCw.jL5qY.O1QtEPBj8LR1a84r0mpuZWzjIPNDaYwm', 'Varga', 'Gabi', '1999-06-21', 'egyeb');
 
 --
 -- Indexes for dumped tables
@@ -65,7 +71,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary kulcs', AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary kulcs', AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
