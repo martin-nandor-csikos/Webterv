54e89e931b634b22ee5bd3ed41ba2e21451130f8
Martin Nándor Csíkos <csikos.martin17@gmail.com>
2023-04-04 12:30:28 +0200

    separated dateValidation from registration

---
+++ php/dateValidation.php
@@ -0,0 +1,10 @@
+<?php
+//A születési dátum validálására szolgáló function
+function datumValidalas ($datum, $format = 'Y.m.d') {
+    //$d változóban létrehozunk egy formátumot
+    $d = DateTime::createFromFormat($format, $datum);
+
+    //ha a létrehozott formátum az megegyezik az átadott dátum formátumával, akkor igaz lesz, ha nem, hamis
+    return $d && $d->format($format) === $datum;
+}
+?>
