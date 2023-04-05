242c90bb894b2e6d6b1697623367f194afe7ce9e
Martin Nándor Csíkos <csikos.martin17@gmail.com>
2023-04-05 16:34:08 +0200

    added redirection

--- php/logout.php
+++ php/logout.php
@@ -6,6 +6,6 @@ $kijelentkezes = $_POST['kijelentkezes'];
 //Ha a kijelentkezés gombra nyomunk, töröljük a sessiont, és visszairányítjuk a usert a főoldalra
 if (isset($kijelentkezes)) {
     stopSession();
-    //header('Location: asd.php');
+    header('Location: ./../loginPage.php');
 }
 ?>
