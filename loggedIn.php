75f9309fe3ce6fa29b2005de124f622d8ca7d516
Martin Nándor Csíkos <csikos.martin17@gmail.com>
2023-04-05 16:32:35 +0200

    added sign out form

---
+++ loggedIn.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bejelentkezett</title>
+
+    <?php
+        require "./php/session.php";
+        startSession();
+        checkSession();
+    ?>
+</head>
+<body>
+    <form action="./php/logout.php" method="POST">
+        <input type="submit" name="kijelentkezes" value="Kijelentkezés">
+        <?php
+            stopSession();
+        ?>
+    </form>
+</body>
+</html>
