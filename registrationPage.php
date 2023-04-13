23bafdbd75d2d93e5909eddc38753b94001d4694
joobgreti <gretoob@gmail.com>
2023-04-13 08:09:02 +0200

    transforming the webshop into a forum

--- registrationPage.php
+++ registrationPage.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 <html lang="hu">
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    require "./php/registration.php";
+}
+?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Regisztráció</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 <form action="./php/registration.php" method="POST">
