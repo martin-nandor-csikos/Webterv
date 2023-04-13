83ba8e9b2cd0696920e0a3a8048bd574ba82fa22
joobgreti <gretoob@gmail.com>
2023-04-13 20:02:26 +0200

    some fixes

--- loginPage.php
+++ loginPage.php
@@ -32,6 +32,7 @@
 
         <!--Menu bar-->
         <nav id="menuBar">
+            <a href="forum.html" id="forumpage" class="notActive">Fórum</a>
             <a href="searchUsersPage.php" id="search" class="notActive">Felhasználók keresése</a>
             <a href="index.html" id="backtoindex" class="notActive">Kilépés a fórumból</a>
             <a href="registrationPage.php" id="registration" class="notActive">Regisztráció</a>
