9baf16602e62fb10693a786a6160859019b9e619
Martin Nándor Csíkos <csikos.martin17@gmail.com>
2023-04-15 18:07:46 +0200

    fixed logout bug (session not stopping)

--- php/logout.php
+++ php/logout.php
@@ -5,6 +5,7 @@ $kijelentkezes = $_POST['kijelentkezes'];
 
 //Ha a kijelentkezés gombra nyomunk, töröljük a sessiont, és visszairányítjuk a usert a főoldalra
 if (isset($kijelentkezes)) {
+    startSession();
     stopSession();
     header('Location: ./../loginPage.php');
 }
