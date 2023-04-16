e0ba81bce16468632c1aff6bde72d4fd0efeca5d
Martin Nándor Csíkos <csikos.martin17@gmail.com>
2023-04-16 20:05:10 +0200

    added viewing other user's profile

--- php/searchUsers.php
+++ php/searchUsers.php
@@ -6,18 +6,38 @@ connect();
 $fhnev = $_GET['fhnev'];
 
 //Megkeressük azokat a felhasználóneveket, amik tartalmazzák a $kereses változót, és a jogosultsága nem admin
-$sqlQuery = "SELECT fhnev FROM users WHERE fhnev LIKE '%$fhnev%' AND jogosultsag <> 'admin'";
+$sqlQuery = "SELECT * FROM users u, userprofile p WHERE u.id = p.userId AND u.fhnev LIKE '%$fhnev%' AND u.jogosultsag <> 'admin'";
 $eredmeny = $csatlakozas->query($sqlQuery);
 
 //Ha van eredmény
 if ($eredmeny->num_rows > 0) {
-    $output = ""; 
+    while ($adat = $eredmeny->fetch_assoc()) {
+        echo "<p class=\"usernames\">" . $adat['fhnev'] . "</p>";
+        
+        if ($adat['bio'] != "") {
+            echo "<p>Bio:<br>" . $adat['bio'] . "</p>";
+        }
 
-    while ($user = $eredmeny->fetch_assoc()) {
-        $output .= "<p class=\"usernames\">" . $user['fhnev'] . "</p>";
+        if ($adat['kernev_publikus'] == 1) {
+            echo "<p>Keresztnév: " . $adat['kernev'] . "</p>"; 
+        }
+
+        if ($adat['veznev_publikus'] == 1) {
+            echo "<p>Vezetéknév: ". $adat['veznev'] . "</p>"; 
+        }
+
+        if ($adat['szuldatum_publikus'] == 1) {
+            echo "<p>Születési dátum: " . $adat['szuldatum'] . "</p>"; 
+        }
+
+        if ($adat['email_publikus'] == 1) {
+            echo "<p>E-mail: " . $adat['email'] . "</p>"; 
+        }
+
+        if ($adat['nem_publikus'] == 1) {
+            echo "<p>Nem: " . $adat['nem'] . "</p>"; 
+        }
     }
-
-    echo $output;
 } else {
     echo "<p>Nincs találat.</p>";
 }
