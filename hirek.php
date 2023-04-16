8a54b0b26b24f39d0c114800b7e34a1eb3caba17
Martin Nándor Csíkos <csikos.martin17@gmail.com>
2023-04-16 19:15:24 +0200

    added userProfile to header

--- hirek.php
+++ hirek.php
@@ -39,10 +39,11 @@
         <nav id="menuBar">
             <a style="padding-left: 60px; padding-right: 60px;" href="#" id="forumpage" class="active">Hírek</a>
             <a style="padding-left: 60px; padding-right: 60px;" href="searchUsersPage.php" id="search" class="notActive">Felhasználók keresése</a>
+            <a style="padding-left: 60px; padding-right: 60px;" href="changeUserInfoPage.php" id="changeinfo" class="notActive">Profil módosítása</a>
+            <a style="padding-left: 60px; padding-right: 60px;" href="userProfile.php" id="" class="notActive">Adatlap</a>
             <a style="padding-left: 60px; padding-right: 60px;" href="index.html" id="backtoindex" class="notActive">Kilépés a fórumból</a>
             <a style="padding-left: 60px; padding-right: 60px;" href="privateMessages.php" class="notActive">Üzenetek</a>
             <a style="padding-left: 60px; padding-right: 60px;" href="logoutPage.php" id="logout" class="notActive">Kijelentkezés</a>
-            <a style="padding-left: 60px; padding-right: 60px;" href="changeUserInfoPage.php" id="changeinfo" class="notActive">Profil módosítása</a>
             <?php
             require "./php/session.php";
             startSession();
@@ -55,7 +56,7 @@
     </header>
 
     <?php
-    echo "<h1>Üdvözöljük, " . $_SESSION['fhnev'] . "!</h1>"; 
+    echo "<h1>Üdvözöljük, " . $_SESSION['fhnev'] . "!</h1>";
     ?>
     <h2 class="title">Hírek</h2>
 
