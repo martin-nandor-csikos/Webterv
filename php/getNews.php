376c75a1c41a0cddacabf4114c2239c877214323
joobgreti <gretoob@gmail.com>
2023-04-16 17:23:33 +0200

    styled getNews.php

--- php/getNews.php
+++ php/getNews.php
@@ -7,9 +7,11 @@ $eredmeny = $csatlakozas->query($sqlQuery);
 
 if ($eredmeny->num_rows > 0) {
     while ($cikk = $eredmeny->fetch_assoc()) {
-        echo "<p>" . $cikk['datum'] . "</p>";
-        echo "<h1>" . $cikk['cim'] . "</h1>";
-        echo "<p>" . $cikk['szoveg'] . "</p>";
+        echo "<div class='news'>";
+        echo "<p id='datum'>" . $cikk['datum'] . "</p>";
+        echo "<h1 id='cim'>" . $cikk['cim'] . "</h1>";
+        echo "<p id='szoveg'>" . $cikk['szoveg'] . "</p>";
+        echo "</div>";
     }
 }
 
