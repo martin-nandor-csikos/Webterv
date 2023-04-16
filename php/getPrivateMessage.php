c86b780ba50c9c245f46da9b3663f39d9410dac2
joobgreti <gretoob@gmail.com>
2023-04-16 17:49:30 +0200

    styled getPrivateMessage.php

--- php/getPrivateMessage.php
+++ php/getPrivateMessage.php
@@ -11,15 +11,19 @@ $eredmeny = $csatlakozas->query($sqlQuery);
 
 //Ha van eredmény
 if ($eredmeny->num_rows > 0) {
+    echo "<div id='messages-container'>";
     //Kiiratjuk az időt, az üzenet küldőjét, címzettét, és az üzenetet
     while ($uzenet = $eredmeny->fetch_assoc()) {
-        echo $uzenet['datetime'] . "<br>";
-        echo "Küldő: " . $uzenet['sentFrom'] . "<br>";
-        echo "Címzett: " . $uzenet['sentTo'] . "<br>";
-        echo "Üzenet: " . $uzenet['message'] . "<br><br>";
+        echo "<div class='message'>";
+        echo "<p id='datetime'>" . $uzenet['datetime'] . "</p>";
+        echo "<p id='sentFrom'>Küldő: " . $uzenet['sentFrom'] . "</p>";
+        echo "<p id='sentTo'>Címzett: " . $uzenet['sentTo'] . "</p>";
+        echo "<p id='message'>Üzenet: " . $uzenet['message'] . "</p>";
+        echo "</div>";
     }
+    echo "</div>";
 } else {
-    echo "Üres a postaládád.";
+    echo "<p class='some'>Üres a postaládád.</p>";
 }
 
 disconnect();
